42358f26fae129546aa8ab01b1fe76c6b56f0d84
Samuel Dubois <sadu6912@colorado.edu>
2023-04-24 17:16:54 -0600

    petCall function and Searchpage implementation

--- appCode/src/index.js
+++ appCode/src/index.js
@@ -63,7 +63,7 @@ app.use(
 
 /*=====Default Route=====*/
 app.get('/', (req, res) => {
-  res.redirect('pages/home');
+  res.redirect('register');
 });
 
 //SAMPLE/TEST API
@@ -76,34 +76,140 @@ app.get('/register', (req, res) =>{
   res.render('pages/register');
 });
 
-// app.post('/register', async (req, res) => {
-// });
 app.post('/register', async (req, res) => {
-  //hash the password using bcrypt library
-  const hash = await bcrypt.hash(req.body.password, 10);
+  //get and validate user data
+  const {username, firstName, lastName, email, password} = req.body;
+  if(!username || !firstName || !lastName || !email ||!password){
+    return res.status(400).json({error : 'One or more fields missing'});
+  }
 
-  // To-DO: Insert username and hashed password into 'users' table
-  const user = req.body.username;
-  const pass = req.body.password;
-  const query = `INSERT INTO users(username, password) VALUES($1, $2) RETURNING username`;
-  db.any(query, [user, hash])
-  .then(function(data) {
-      res.redirect('/login');
-  })
-  .catch(function (err) {
-      console.log(err);
-      res.redirect('/register');
-  });
+  //check if user alrady exists
+  const checkQuery = await db.oneOrNone(`SELECT * FROM users WHERE username = $1;`, [username]);
+  if(checkQuery){ //if user already in data base
+    return res.status(400).json({error : 'User already exists'});
+  }
+
+  //hash the password using bcrypt library
+  const hash = await bcrypt.hash(password, 10);
+
+  //adding new user to DB
+  try{
+    const insertQuery = 'INSERT INTO users (username, email, firstName, lastName, password) VALUES ($1, $2, $3, $4, $5);'
+    await db.none(insertQuery, [username, email, firstName, lastName, hash]);
+    res.status(200).send('Registration Success!')
+    // res.redirect('login');
+  }catch (error){
+    console.error(error);
+    res.status(500).json({error : 'Server Error'});
+  }
 });
 
 
 /*=====Login APIs=====*/
 app.get('/login', (req,res) =>{
-res.render('pages/login');
+  res.render('pages/login');
 });
 
-// app.post('login', async (req, res) => {
-// });
+app.post('/login', async (req, res) => {
+  //get and validate user data
+  const {username, password} = req.body;
+  if(!username || !password){
+    return res.status(400).json({error : 'One or more fields missing'});
+  }
+
+  //find given user in DB
+  const query = `SELECT password FROM users where username = $1;`;
+  const passwordDB = await db.oneOrNone(query, [username]);
+  if(!passwordDB){ //if user not found
+    res.status(404).send('User not found');
+  } 
+  else{
+    //comparing given password and password from DB
+    await bcrypt.compare(password, passwordDB.password, (err, result) =>{
+      if(err){
+        console.error(err);
+      }
+      else{
+        if(!result){ //passwords dont match
+          res.status(401).json({error : 'Incorrect password'});
+        }
+        else{
+          //setting session variable
+          req.session.user = username;
+          req.session.save();
+
+          res.status(200).send('Login Success!');
+          // res.redirect('home');
+        }
+      }
+    });
+  }
+});
+
+
+/*=========Get Dogs API Call=========*/
+
+var pets = {};
+
+pets.apiKey ="OCvLf7EtbC6ZY84CReHeoDhSqHBavqz0kkdFy1St2rT1qliBNI";
+pets.apiSecret ="zP8vSDaUvihfyhPTJ2IsZPy26I3qox1Ifw0bhAnQ";
+pets.apiToken = curl -d "grant_type=client_credentials&client_id={OCvLf7EtbC6ZY84CReHeoDhSqHBavqz0kkdFy1St2rT1qliBNI}&client_secret={zP8vSDaUvihfyhPTJ2IsZPy26I3qox1Ifw0bhAnQ}" https://api.petfinder.com/v2/oauth2/token;
+pets.petUrl = "https://api.petfinder.com/pet.find";
+pets.availablePets = $('#availablePets');
+
+pets.form = function() {
+	$('#petForm').on('submit', function(e){
+		e.preventDefault();
+		var userLocation = $('.currentLocation').val();
+    var petType = $('select#petType option:checked').val();
+    var petSex = $('select#petSex option:checked').val();
+    console.log('click');
+		pets.petsCall(userLocation, petType, petSex);
+	});
+}
+
+pets.petsCall = function(userLocation, petType, petSex) {
+	console.log(userLocation, petType, petSex);
+	$.ajax({
+		url: pets.petUrl,
+		method: 'GET',
+    crossDomain: true,
+		dataType: 'jsonp',
+		data : {
+			key: pets.apiKey,
+			location: userLocation,
+      animal: petType,
+      sex: petSex,
+			format: 'json',
+			count: 10,
+			age: 'Senior',
+			status: 'A'
+		}  
+	}).then(function(results){
+  	var petResults = results.petfinder.pets.pet;
+		console.log(petResults);
+		for (var i = 0; i < petResults.length; ++i) {
+    	var petName = petResults[i].name.$t;
+      var petPhoto = petResults[i].media.photos.photo[0].$t;
+      console.log(petName);
+      console.log(petPhoto);
+    	pets.availablePets.append('<p>' + petName + '</p>');
+      pets.availablePets.append('<div><img src="' + petPhoto + '"></div>')
+  	}
+	});
+};
+
+$(document).ready(function() {
+	pets.form();
+});
+/*=====Authentication Middleware=====*/
+const auth = (req, res, next) => {
+  if(!req.session.user){
+    return res.redirect('login');
+  }
+  next();
+};
+app.use(auth);
 
 
 // *****************************************************
@@ -112,3 +218,5 @@ res.render('pages/login');
 // starting the server and keeping the connection open to listen for more requests
 app.listen(3000);
 console.log('Server is listening on port 3000');
+
+module.exports = app;
