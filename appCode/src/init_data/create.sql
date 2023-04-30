4cf959d64ee6c8e0a53276e977d49929865a724c
Jackson Cockrum <jaco7466@colorado.edu>
2023-04-30 15:57:10 -0600

    updated home page(adoption resources are hardcoded), working on adding profile image

--- appCode/src/init_data/create.sql
+++ appCode/src/init_data/create.sql
@@ -6,8 +6,14 @@ CREATE TABLE IF NOT EXISTS users(
     email VARCHAR(50)
 );
 
--- CREATE TABLE IF NOT EXISTS profileImages(
---     username VARCHAR(50),
---     image_data BLOB,
---     FOREIGN KEY (username) REFERENCES users(username)
+-- CREATE TABLE IF NOT EXISTS articles(
+--     id SERIAL PRIMARY KEY UNIQUE,
+--     article_name VARCHAR(1000),
+--     link VARCHAR(1000)
 -- );
+
+-- INSERT INTO articles (article_name, link) 
+--     VALUES ('Adopting a Rescue Dog: The Ins and Outs and What to Expect', 'https://www.aspcapetinsurance.com/resources/ins-and-outs-of-adopting-a-dog/'),
+--     ('General Dog Care','https://www.aspca.org/pet-care/dog-care/general-dog-care'),
+--     ('6 Winter Safety Tips for Dogs in the Snow', 'https://www.purina.com/articles/dog/care/winter-safety-tips-for-dogs-in-snow'),
+--     ('14 Tips for First-Time Dog Owners', 'https://www.dailypaws.com/dogs-puppies/dog-adoption/first-time-dog-owner-tips');
